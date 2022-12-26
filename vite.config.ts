c1823f6d21faa44a254968a1ef3cd286b13124f3
Hatton <hattonjohn@gmail.com>
2022-12-26 16:20:56 -0700

    Upgrade  vite-electron-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   plugins: [
     react(),
     electron({
-      include: ["electron", "preload"],
+      include: ["electron"],
       transformOptions: {
         sourcemap: !!process.env.VSCODE_DEBUG,
       },
